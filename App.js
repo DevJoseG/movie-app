6a4a83a2e552354fa461b70258ef082eb154b4b5
Jose Gutierrez <139079654+DevJoseG@users.noreply.github.com>
2023-07-09 18:26:51 -0400

    He cargado el proyecto completo

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import AppNavigation from './navigation/appNavitagion';
+
+export default function App() {
+  return (
+    <AppNavigation />
+  );
+}
+
